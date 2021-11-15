3feed9b19aa0ae33ba1b2484ab5923722bc2ef7f
matsegard <matsegard@outlook.com>
2021-11-15 13:23:05 +0100

    Added start page

--- main.js
+++ main.js
@@ -4,8 +4,17 @@ function main(){
   addEventListener();
 }
 
-function addEventListener () {
-    const 
+function addEventListener() {
+    const startButton = document.getElementById('start-button');
+    startButton.onclick = startGame;
+}
+
+function startGame(){
+    const startGame = document.getElementById('first-page');
+  
+    if(startGame.style.display === 'block') {
+    } else {startGame.style.display = 'none'}
+    startGame =  prompt('Skriv in ditt namn');
 }
 
 
