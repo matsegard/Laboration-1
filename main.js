160e671380ce3aa0ae82be9e02eb206fde2e5794
matsegard <matsegard@outlook.com>
2021-11-16 20:13:15 +0100

    Added change text and image

--- main.js
+++ main.js
@@ -1,50 +1,70 @@
+const buttons = {
+  "startbutton": document.getElementById('start-button'),
+  "digbutton": document.getElementById('dig-story'),
+  "riotbutton": document.getElementById('riot-story')
+}
+
+const pages = {
+  "startPage": document.getElementById('first-page'),
+  "secondPage": document.getElementById('second-page'),
+  "thirdPage": document.getElementById('second-page')
+}
+const extraChanges = {
+  "h2Change": document.getElementById('headerText'),
+  "imgChange": document.getElementById('prisoner')
+}
+
 // LADDAR IN MAIN FUNKTION
 window.onload = main;
 
-// main funktionern lägger in listerner
 function main() {
+  buttons.riotbutton.style.display = "block"
+  buttons.digbutton.style.display = "block"
   addEventListener();
 }
 
 function addEventListener() {
-  const startButton = document.getElementById('start-button');
-  startButton.onclick = start;
-
-  const digButton = document.getElementById('dig-story')
-  digButton.onclick = digYourWayOut;
+  buttons.startbutton.onclick = start;
 }
 
 function start() {
-  const startGame = document.getElementById('first-page');
-
-  if (startGame.style.display === 'block') {
+  if (pages.startPage.style.display === 'block') {
 
   } else {
-    startGame.style.display = 'none'
+    pages.startPage.style.display = 'none'
   }
   chooseWayOut();
 }
 
-function chooseWayOut() {
-  const firstChoice = document.getElementById('second-page');
 
-  if (firstChoice.style.display === 'none') {
+function chooseWayOut() {
+  if (pages.secondPage.style.display === 'none') {
 
   } else {
-    firstChoice.style.display = 'block'
+    pages.secondPage.style.display = 'block'
   }
-  digYourWayOut();
 }
 
-function digYourWayOut() {
-
-  const digging = document.getElementById('third-page');
-
-  if (digging.style.display === 'none') {
-
-  } else {
-    digging.style.display = 'block'
+function changeText(item){
+  if(item.id === "dig-story") {
+    extraChanges.h2Change.innerHTML = "Du stjäl en sked från matsalen och börjar gräva en liten tunnel bakom toaletten i din cell. Du har nu 30år av grävande framför dig.";
+  }else if(item.id === "riot-story") {
+    extraChanges.h2Change.innerHTML = "Du startar ett upplopp i fängelset genom att ta en fångvaktare som gisslan. Gisslandramat leder till ett upplopp och du lyckas klättra över muren under allt tumult.";
   }
-  console.log('test');
+}
+
+function changeImage(item) {
+  if(item.id === "dig-story") {
+    extraChanges.imgChange.src = "./images/spoon.png"
+  }else if(item.id === "riot-story") {
+    extraChanges.imgChange.src = "./images/hostage.png"
+  }
+}
+
+function imgtextChange(item) {
+  changeText(item)
+  changeImage(item)
+  buttons.digbutton.style.display = "none"
+  buttons.riotbutton.style.display = "none"
 }
 
