188de4f1656a12842be24207350c9374800199cd
matsegard <matsegard@outlook.com>
2021-11-16 12:27:47 +0100

    update

--- main.js
+++ main.js
@@ -10,6 +10,10 @@ function main() {
 function addEventListener() {
   const startButton = document.getElementById('start-button');
   startButton.onclick = startGame;
+
+  
+  const digYourWayOut = document.getElementById('dig-story');
+  digYourWayOut.onclick = digYourWayOutStory;
 }
 
 function startGame() {
@@ -24,13 +28,13 @@ function startGame() {
 }
 
 /** Input name in prompt */
-function prepareStory(){
-  const answer = prompt('Skriv in ditt namn');
-  alert('Hej ' + answer + '! Här börjar vägen ut till friheten. Du befinner dig i din cell och får nu välja hur du vill ta dig ut ur fängelset.');
+function prepareStory() {
+  // const answer = prompt('Skriv in ditt namn');
+  // alert('Hej ' + answer + '! Här börjar vägen ut till friheten. Du befinner dig i din cell och får nu välja hur du vill ta dig ut ur fängelset.');
   chooseWayOut();
 }
 
-function chooseWayOut(){
+function chooseWayOut() {
   const firstChoice = document.getElementById('second-page');
 
   if (firstChoice.style.display === 'none') {
@@ -38,14 +42,12 @@ function chooseWayOut(){
   } else {
     firstChoice.style.display = 'block'
   }
+  digYourWayOutStory();
 }
 
-function digYourWayOut(){
-  const digYourWayOut = document.getElementsById('riot-story');
-  console.log();
+function digYourWayOutStory() {
+  let secondPage = document.getElementById('second-page')
+  let digYourWayOut = document.getElementById('third-page');
+  secondPage.style.display = "none"
+  digYourWayOut.style.display = "block"
 }
-
-
-
-
-
